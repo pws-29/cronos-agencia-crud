f59088614540660f871d3f8263ea836776ce94e6
Pietro Weg <pietrows29@gmail.com>
2021-12-27 18:55:52 -0300

    Recuperando valores do formulário

--- index.js
+++ index.js
@@ -14,9 +14,61 @@ let Modal = {
 }
 // Ações de Modal
 
-// Local Storage
+// Local Storage API
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("cronos.cursos:cursos")) || [];
+  },
 
+  set() {
+    localStorage.setItem("cronos.cursos:cursos", JSON.stringify(cursos)); // Array -> String
+  }
+}
+// Local Storage API
 
-// Local Storage
+// Lista Cursos
+let cursos = [
+  {
+    id: 1,
+    nome: "Desenvolvimento Web",
+    descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti.",
+    imagem: "./imagens/ilustra-web.png",
+  },
+  {
+    id: 2,
+    nome: "Marketing Digital",
+    descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti.",
+    imagem: "./imagens/ilustra-marketing.png",
+  },
+  {
+    id: 3,
+    nome: "Consultoria UX",
+    descricao: "Consequatur debitis ipsa numquam illum placeat quod deleniti.",
+    imagem: "./imagens/ilustra-ux.png",
+  },
+]
+// Lista Cursos
 
+// Informações formulário
+const Form = {
+  getValues() {
+    return {
+      nome: document.querySelector("#name").value,
+      descricao: document.querySelector("#description").value,
+      imagem: document.querySelector('input[name="input-img"]:checked').value,
+    }
+  },
 
+  submit(event) {
+    // Alterar comportamento padrão do submit
+    event.preventDefault();
+
+    // Buscar valores do Form
+    const Cursos = Form.getValues();
+    // Fechar o modal
+    Modal.close();
+    console.log(Cursos);
+  }
+}
+
+// Informações formulário
